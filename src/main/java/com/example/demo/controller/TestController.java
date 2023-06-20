35dbf588e8573113dec23b7631b690b845b7d19f
Yoo Ji Won <cherixy1128@naver.com>
2023-06-20 20:59:57 +0900

    백엔드 개발 14. ResponseEntity로  HTTP status 조작하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,37 +1,55 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList; 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.ResponseDTO;
-import com.example.demo.dto.TestRequestBodyDTO;
 
+import com.example.demo.dto.TestRequestBodyDTO;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
 @RequestMapping("test")
 public class TestController {
-	
-	@GetMapping
+@GetMapping
 	public String TestController(){
 		return "Hello World!";
 	}
 	@GetMapping("/testRequestBody")
-	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO){
+	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO
+testRequestBodyDTO){
 		return "Hello World! ID"+testRequestBodyDTO.getId()+" Message : "+ testRequestBodyDTO.getMessage();
-	}
-	@GetMapping("/testResponseBody")	
+}
+	@GetMapping("/testResponseBody")
 	public ResponseDTO<String>testControllerResponseBody(){
 		List<String> list =new ArrayList<String>();
 		list.add("Hello World! I'm ResponseDTO");
-		list.add("See you!"
-				);
+		list.add("See you!");
+		
+		ResponseDTO<String> response =
+				ResponseDTO.<String>builder().data(list).build();
+		return response;
+	}
+	@GetMapping("/testResponseEntityOk")
+	public ResponseEntity<?>testControllerResponseEntityOk(){
+		List<String> list =new ArrayList<String>();
+		list.add("Hello World! I'm ResponseEntity. And you get 200!");
+		list.add("See you!");
 		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
-			return response;
-		}
+			return ResponseEntity.ok().body(response);
+	}
+	@GetMapping("/testResponseEntityBad")
+	public ResponseEntity<?>testControllerResponseEntityBad(){
+		List<String> list =new ArrayList<String>();
+		list.add("Hello World! I'm ResponseEntity. And you get 400!");
+		list.add("See you!");
+		ResponseDTO<String> response =ResponseDTO.<String>builder().data(list).build();
+		return ResponseEntity.badRequest().body(response);
+	}
 }
