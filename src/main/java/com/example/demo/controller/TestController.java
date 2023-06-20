82f881e84dfa6da626b980f29649bbc1848f9793
Yoo Ji Won <cherixy1128@naver.com>
2023-06-20 21:23:41 +0900

    백엔드 개발 16. @Repository 사용하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -8,23 +8,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.service.TodoService;
-
 
 @RestController
 @RequestMapping("todo")
 public class TestController {
-
+	
 	@Autowired
 	private TodoService service;
-	
 	@GetMapping("/test")
 	public ResponseEntity<?>testTodo(){
 		String str = service.testService();
 		List<String> list =new ArrayList<>();
 		list.add(str);
-		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		ResponseDTO<String> response = 
+		ResponseDTO.<String>builder().data(list).build();
 		return ResponseEntity.ok().body(response);
 	}
 }
--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,10 +1,23 @@
 package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.model.TodoEntity;
+import com.example.demo.persistence.TodoRepository;
 
 @Service
 public class TodoService {
+
+	@Autowired
+	private TodoRepository repository;
 	public String testService(){
-		return "Test Service";
+
+	TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+	
+	repository.save(entity);
+
+	TodoEntity savedEntity = repository.findById(entity.getId()).get();
+		return savedEntity.getTitle();
 	}
 }
