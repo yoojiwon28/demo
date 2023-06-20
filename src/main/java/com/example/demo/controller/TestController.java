502d95eb53edf44821398f5eecd349cdfd597e0e
Yoo Ji Won <cherixy1128@naver.com>
2023-06-20 20:46:50 +0900

    백엔드 개발 13. @ResponseBody 사용하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList; 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TestRequestBodyDTO;
 
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,10 +21,17 @@ public class TestController {
 	public String TestController(){
 		return "Hello World!";
 	}
-	
 	@GetMapping("/testRequestBody")
-	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO)
-	{
+	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO){
 		return "Hello World! ID"+testRequestBodyDTO.getId()+" Message : "+ testRequestBodyDTO.getMessage();
 	}
+	@GetMapping("/testResponseBody")	
+	public ResponseDTO<String>testControllerResponseBody(){
+		List<String> list =new ArrayList<String>();
+		list.add("Hello World! I'm ResponseDTO");
+		list.add("See you!"
+				);
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+			return response;
+		}
 }
